863c9a1c1742a6f5635ab64648bf8c84b2fa105b
Jason Qu <fishinsea2014@gmail.com>
2018-06-01 13:03:21 +1200

    The function of hiding the children of a node when clicking do not work properly.

--- menuTree.js
+++ menuTree.js
@@ -35,22 +35,27 @@ var data = [
     ];
 
     $(function(){
-      createMenuTree(data);
+      var _div = $('#menuTree');
+      menuTree.init(_div, data);
+
+
+      // createMenuTree(data);
     })
 
-    var _that = this;
+    var menuTree = {
+      init: function (_dom, data){
+        this.createMenuTree(_dom,data);
+        this.bind(_dom);
+      },
 
-
-    function createMenuTree(data){
-      var html='';
+      createMenuTree: function (_dom, data){
+        var html='';
         let n=0;
-        function makeMenu(data,level,curNode) {
-          
+        function makeMenu(data,level,curNode) {          
             n++;            
             for (k in data) {
-                if (data[k].name=='a2') console.log(data[k])
                 let leftPadding=level*0.6;
-                curNode.append(`<li class="menuItem ${n}" menu-level="${level}" style="padding-left:${leftPadding}em" onclick="_that.toggleChild(${n})">+${data[k].name}</li>`);                
+                curNode.append(`<li class="menuItem ${n}" menu-level="${level}" style="padding-left:${leftPadding}em" >+${data[k].name}</li>`);                
                 if (data[k].child !='undefined') {
                   makeMenu(data[k].child, level+1,$(`.menuItem.${n}`));
                 } else {
@@ -58,18 +63,71 @@ var data = [
                 }
             }
         }
+        makeMenu(data,0,_dom);
+      },
 
-        makeMenu(data,0,$('#menuTree'));
+      bind: function(_dom){
+        var _that = this;
+        _dom.on("click", function(){
+          console.log('click a menu item');
+          if (event.target.localName == 'li' && event.target.children.length >0){
+            var pnode = event.target.parentNode;
+            _that.hideNode(pnode);
+            event.target.children[0].style.display = 'block';
+          }
+          // debugger;
+        })
+      },
+
+      hideNode: function (pnode) {
+        console.log('hidenode');
+        var nodes = pnode.children;
+        // debugger;
+
+        for (let i=0; i<nodes.length; i++) {
+          if (nodes[i].children.length > 0) {
+            nodes[i].children[0].style.display = 'none';
+            // debugger;
+            this.hideNode(nodes[i].children[0]);
+          }
+        }       
+      }
     }
+
+
+
+    // var _that = this;
+
+
+    // function createMenuTree(data){
+    //   var html='';
+    //     let n=0;
+    //     function makeMenu(data,level,curNode) {
+          
+    //         n++;            
+    //         for (k in data) {
+    //             if (data[k].name=='a2') console.log(data[k])
+    //             let leftPadding=level*0.6;
+    //             curNode.append(`<li class="menuItem ${n}" menu-level="${level}" style="padding-left:${leftPadding}em" onclick="_that.toggleChild(${n})">+${data[k].name}</li>`);                
+    //             if (data[k].child !='undefined') {
+    //               makeMenu(data[k].child, level+1,$(`.menuItem.${n}`));
+    //             } else {
+    //               return false;
+    //             }
+    //         }
+    //     }
+
+    //     makeMenu(data,0,$('#menuTree'));
+    // }
     
 
 
-    //？点击某个菜单项的时候，调用这个方法，把这个菜单项下的children都给隐藏，但是没有成功
-    function toggleChild(id){
-      console.log($(`li.menuItem.${id}`));
-      $(`li.menuItem.${id}`).children().toggle();
+    // //？点击某个菜单项的时候，调用这个方法，把这个菜单项下的children都给隐藏，但是没有成功
+    // function toggleChild(id){
+    //   console.log($(`li.menuItem.${id}`));
+    //   $(`li.menuItem.${id}`).children().toggle();
       
-    }
+    // }
 
 
 
